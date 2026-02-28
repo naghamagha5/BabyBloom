676eb290ed67d971f756abb2e0abafa38053640c
alhassen agha <alhassenhamdy@yahoo.com>
2026-02-28 15:45:34 +0200

    Add splash screen support and updated app assets

--- app/src/main/java/com/babybloom/ui/theme/Color.kt
+++ app/src/main/java/com/babybloom/ui/theme/Color.kt
@@ -9,3 +9,17 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val DeepNavy = Color(0xFF1A1F3C)          // Home dark background, bottom nav
+val SoftLavender = Color(0xFFEDE7F6)      // Register/Login background
+val WarmPeach = Color(0xFFFFF3E0)         // Add child background
+val PrimaryPurple = Color(0xFF7B5EA7)     // Buttons, accents
+val AccentTeal = Color(0xFF4DB6AC)        // Overview cards
+val AccentOrange = Color(0xFFFF8A65)      // Achievement cards
+val AccentPink = Color(0xFFF48FB1)        // Hearts, soft accents
+val CardDark = Color(0xFF252A4A)          // Dark cards on home screen
+val TextOnDark = Color(0xFFFFFFFF)
+val TextOnLight = Color(0xFF2D2D2D)
+val TextSecondary = Color(0xFF757575)
+val InputBackground = Color(0xFFF5F5F5)
