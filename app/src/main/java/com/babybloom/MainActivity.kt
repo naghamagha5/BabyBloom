b9f3249111109b4c1842736f88d33e3f91b0d941
alhassen agha <alhassenhamdy@yahoo.com>
2026-06-12 23:37:11 +0300

    adjust the app to arabic phons

--- app/src/main/java/com/babybloom/MainActivity.kt
+++ app/src/main/java/com/babybloom/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.babybloom
 
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -21,6 +23,7 @@ import com.babybloom.di.SessionManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import com.babybloom.navigation.BabyBloomNavGraph
+import java.util.Locale
 import javax.inject.Inject
 
 
@@ -33,6 +36,15 @@ class MainActivity : ComponentActivity() {
     @Inject lateinit var seeder: DatabaseSeeder
     @Inject lateinit var learningContentSeeder: LearningContentSeeder
     @Inject lateinit var activitySeeder: ActivitySeeder
+
+    override fun attachBaseContext(newBase: Context) {
+        val configuration = Configuration(newBase.resources.configuration).apply {
+            setLocale(Locale.ENGLISH)
+            setLayoutDirection(Locale.ENGLISH)
+        }
+        super.attachBaseContext(newBase.createConfigurationContext(configuration))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen().apply {
